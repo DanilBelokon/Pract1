3ca2a79c66393ff2f6914cc52c95d9dae9787056
Danil <ilya.belokon.07@gmail.com>
2025-02-07 01:09:48 +0300

    второй коммит

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,31 @@ const personalMovieDB = {
   privat: false,
 };
 
-const nameCinema = prompt("Один из просмотренных фильмов?", "");
-const golCinema = prompt("На сколько его оцените?", "");
+for (let i = 0; i < 2; i++) {
+  let nameCinema = prompt("Один из просмотренных фильмов?", "");
+  if (nameCinema == "" || nameCinema == null || nameCinema.length > 50) {
+    console.log("1");
+    while (nameCinema == "" || nameCinema == null || nameCinema.length > 50) {
+      nameCinema = prompt("Один из просмотренных фильмов?", "");
+    }
+  }
+  let golCinema = prompt("На сколько его оцените?", "");
+  if (golCinema == "" || golCinema == null) {
+    while (golCinema == "" || golCinema == null) {
+      golCinema = prompt("На сколько его оцените?", "");
+    }
+  }
+  personalMovieDB.movies[nameCinema] = golCinema;
+}
 
-personalMovieDB.movies[nameCinema] = golCinema;
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено добольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else {
+  alert("Вы киноман");
+}
+
 console.log(personalMovieDB.movies);
 // personalMovieDB["movies"[prompt("Один из просмотренных фильмов?", "")]] = prompt(
 //   "На сколько его оцените?",
